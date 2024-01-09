72f587ed3cf5bafd98514bba83549876391f0683
Robert <Roberttsax18@gmail.com>
2024-01-09 12:25:17 -0500

    As much as what made sense

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+// START YOUR SERVER HERE
+
 const server = require('./api/server');
 
 const port = 9000;
 
-// START YOUR SERVER HERE
+server.listen(port, () => console.log('API RUNNING!'));
+
+
+
+
+
